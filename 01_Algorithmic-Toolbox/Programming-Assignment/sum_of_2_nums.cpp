bfd91b478dd8e4b9d2a8389af9006dc20a56b772
P Ganesh <nagpichikaganesh@gmail.com>
2024-09-25 00:05:17 +0530

    module_01_pa1

--- 01_Algorithmic-Toolbox/Programming-Assignment/sum_of_2_nums.cpp
+++ 01_Algorithmic-Toolbox/Programming-Assignment/sum_of_2_nums.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 int main(){
     int a, b;
-    cout << "a: ";
-    cin >> a;
-    cout << "b: ";
-    cin >> b;
-    cout << "sum:" << a + b;
+    cin >> a >> b;
+    cout << a + b;
     return 0;
 }
