7b05f372a8161ddbf680ff459c3e871d3c7bf064
P Ganesh <nagpichikaganesh@gmail.com>
2024-09-24 22:50:13 +0530

    module_01_pa1

---
+++ 01_Algorithmic-Toolbox/01_Programming-Assignment directory/01_sum-of-2-nums.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    cout << "sum:" << a + b;
+    return 0;
+}
